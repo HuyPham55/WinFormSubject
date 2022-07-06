9f6dbbbc51621319ca2bcd2300a479c53dc6f6a9
huydu <huyducphamofficial@gmail.com>
2022-07-06 18:37:30 +0700

    Feat: Update DataBaseConnection boilerplate

---
+++ CodeBase/DatabaseConnection.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBase
+{
+    class DatabaseConnection
+    {
+        public SqlConnection cnn;
+        public SqlCommand cmd;
+        public DataTable dta;
+        public SqlDataAdapter ada;
+
+        public void LetsConnect()
+        {
+            string connectionKey = @"Data Source=DESKTOP-QAKVRQJ;Initial Catalog=PHAMDUCHUY;Integrated Security=True";
+            cnn = new SqlConnection(connectionKey);
+            cnn.Open();
+        }
+        public void CloseMyConnection()
+        {
+            if (cnn.State == ConnectionState.Open)
+            {
+                cnn.Close();
+            }
+        }
+
+        public DataTable GetData(string sql)
+        {
+            LetsConnect();
+
+            ada = new SqlDataAdapter(sql, cnn);
+            dta = new DataTable();
+            ada.Fill(dta);
+            return dta;
+        }
+
+      
+        public void ExecuteQuery(string query)
+        {
+            LetsConnect();
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            cmd.ExecuteNonQuery();
+            CloseMyConnection();
+        }
+
+        public void ResultExist(string query)
+        {
+            SqlCommand cmd = new SqlCommand(query, cnn);
+            SqlDataReader flag = cmd.ExecuteReader();
+            return flag.Read();
+        }
+    }
+}
